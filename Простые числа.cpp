1cc6168128a7777b394f8b4b0f29ae7f3e733ac5
antonandronchik <antonandron46@gmail.com>
2016-10-20 20:51:19 +0300

    Update Простые числа.cpp

--- Простые числа.cpp
+++ Простые числа.cpp
@@ -2,29 +2,45 @@
 
 using namespace std;
 
-bool isSimpleNumber(int);
+bool isSimpleNumber(long long);
+
+void PrintSimpleNumber(long long);
 
 int main() {
-	int n = 2, a;
-	cout << "Please, enter diapazon" << endl;
-	cin >> a;
-	while (a < 1) {
-		cout << "Please enter a positive number" << endl;
+	short ok = true;
+	while (ok) {
+		long long n = 2, a;
+		cout << "Please, enter diapazon\n";
 		cin >> a;
+		while (a < 2) {
+			cout << "Please enter other diapazon\n";
+			cin >> a;
+		}
+		PrintSimpleNumber(a);
+		cout << "If you want to continue press 1, exit press 0\n";
+		cin >> ok;
+		if (ok == 0)
+			return 0;
+		if (ok == 1)
+			system("cls");
+		else return 0;
 	}
-	while (n <= a) {
-		if (isSimpleNumber(n))
-				cout << n << "  ";
-		n++;
-	} 
-	cout << endl;
-	return 0;
 }
 
-bool isSimpleNumber(int n) {
-	int i, res;
+bool isSimpleNumber(long long n) {
+	int i;
 	for (i = 2; i <= sqrt(n); i++)
 		if (!(n % i))
 			return false;
 	return true;
+}
+
+void PrintSimpleNumber(long long a) {
+	int n = 2;
+	while (n <= a) {
+		if (isSimpleNumber(n))
+			cout << n << "  ";
+		n++;
+	}
+	cout << endl;
 }
