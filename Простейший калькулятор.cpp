e6e7b2f7b71e9a7be20d93a249f1cc190e3fd39f
antonandronchik <antonandron46@gmail.com>
2016-10-22 16:01:33 +0300

    Update Простейший калькулятор.cpp

--- Простейший калькулятор.cpp
+++ Простейший калькулятор.cpp
@@ -2,50 +2,31 @@
 
 using namespace std;
 
-int main()
-
-{
+int main() {
 	cout << "Welcome to calculator" << endl;
-
 	cout << "Enter * or . if multiplication" << endl;
-
 	cout << "Enter / or : if division" << endl;
-
 	cout << "Enter + if addition" << endl;
-
 	cout << "Enter - if subtraction" << endl;
-
 	char operation;
-
 	cin >> operation;
-
 	cout << "Enter numbers" << endl;
-
 	double a, b;
-
-	
 	cin >> a >> b;
-
 	double c;
-
 	bool ok = true;
-
 	switch (operation)
 	{
-
 	case '*':
 	case '.':
 		c = a*b;
 		break;
-
 	case '-': 
 		c = a-b;
 		break;
-
 	case '+': 
 		c = a+b;
 		break;
-
 	case '/':
 	case ':':
 		if (b == 0)
@@ -57,12 +38,8 @@ int main()
 		ok = false;
 	}
 	if (ok = true)
-
 		cout << a << " " << operation << " " << b << " = " << c << endl;
-
 	else 
-
 		cout << "Input error!" << endl;
-
 	return 0;
 }
