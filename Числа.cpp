75fadd00a12da7cf5fde0bc2469a026b4934d038
antonandronchik <antonandron46@gmail.com>
2016-10-22 16:39:25 +0300

    Update and rename Numbers.cpp to Числа.cpp

--- Числа.cpp
+++ Числа.cpp
@@ -1,26 +1,39 @@
 #include <iostream>
+
 using namespace std;
+
 int MinDigit(long long);
+
 int MaxDigit(long long);
+
 int main() {
 	int i;
-	long long n, a;
+	long long n, a, c;
 	cout << "Please enter number" << endl;
 	cin >> n;
 	a = n;
+	c = n;
 	if (n == 0) {
 		cout << "Input Error!" << endl;
-		return 1;
+		return 0;
 	}
-	for (i = 0; a ; i++)
-		a /= 10;
-	cout << "The amount of digits of the input number is " << i << endl;
+		for (i = 0; i <= 9; i++) {
+			int z = 0;
+			int buff = c;
+			while (buff) {
+				int ost = buff % 10;
+				if (ost == i)
+				z++;
+				buff /= 10;
+			}
+			cout << " There " << z << " times of the number " << i << endl;
+		}
 	cout << "The min digit of the input number is " << MinDigit(fabs(n)) << endl;
 	cout << "The max digit of the input number is " << MaxDigit(fabs(n)) << endl;
 	return 0;
 }
-int MinDigit(long long n)
-	 {
+
+int MinDigit(long long n) {
 		int cur;
 		int min_n = 9;
 		while (n) {
@@ -30,8 +43,8 @@ int MinDigit(long long n)
 		}
 		return min_n;
 	} 
-int MaxDigit(long long n)
-	{
+
+int MaxDigit(long long n) {
 		int cur;
 		int max_n = 0;
 		while (n) {
