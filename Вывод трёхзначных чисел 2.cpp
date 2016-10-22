bbee9efe669cfbbd274c6943cac4183e81bda6fd
antonandronchik <antonandron46@gmail.com>
2016-10-22 17:35:47 +0300

    Update Вывод трёхзначных чисел 2.cpp

--- Вывод трёхзначных чисел 2.cpp
+++ Вывод трёхзначных чисел 2.cpp
@@ -1,23 +1,36 @@
 #include <iostream>
 
-#include <cmath>
-
 using namespace std;
 
+bool sumCubeNumbers(int);
+
+void PrintNumbers(int);
+
 int main() {
-	int i = 100;
-	int copy, a, b, c;
-	cout <<"All three-digit numbers that are equal to the sum of the cubes of its digits\n";
-	while(i < 1000) 
-	{
-		copy = i;
-		a = copy % 10;  // a - последняя цифра в числе
-		b = (copy/10) % 10;  // b - вторая цифра в числе
-		c = (copy/100) % 10;  // c - перввая цифра в числе
-			if (pow(a, 3.0) + pow(b, 3.0) + pow(c, 3.0) == i) 
-				cout<< i << "  ";
-		i++;
-	}
-	cout<<endl;
+	int num = 100;
+	cout << "All three-digit numbers that are equal to the sum of the cubes of its digits\n";
+	PrintNumbers(num);
 	return 0;
+}
+
+bool sumCubeNumbers(int num) {
+	int copy, a, b, c;
+	while (num < 1000) {
+		copy = num;
+		a = copy % 10;
+		b = (copy / 10) % 10;
+		c = (copy / 100) % 10;
+		if (pow(a, 3.0) + pow(b, 3.0) + pow(c, 3.0) == num)
+			return true;
+		else return false;
+	}
+}
+
+void PrintNumbers(int num) {
+	while (num < 1000) {
+		if(sumCubeNumbers(num))
+		cout << num << "  ";
+		num++;
+	}
+cout << endl;
 }
