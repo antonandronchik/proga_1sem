56e1f592c094d06dd951540b4fb05558df8a2632
antonandronchik <antonandron46@gmail.com>
2016-10-22 17:45:30 +0300

    Update Палиндром.cpp

--- Палиндром.cpp
+++ Палиндром.cpp
@@ -4,29 +4,35 @@ using namespace std;
 
 bool isPalindrome(long long);
 
+void PrintPalindrome(long long, long long);
+
 int main() {
 	long long n = 10, a;
-		cout << "Please, enter diapazon" << endl;
-		cin >> a;
-		while(n <= a){
-		if (isPalindrome(n))
-			cout << n << "  ";
-		n++;
-		}
-			cout<<endl;
-			return 0;
-	}
+	cout << "Please, enter diapazon" << endl;
+	cin >> a;
+	PrintPalindrome(a, n);
+	return 0;
+}
 
 bool isPalindrome(long long n) {
 	long long cop, rev = 0;
 	cop = abs(n);
-		rev = 0;
-		while (cop) {
-			short a = cop % 10;
-			rev = rev * 10 + a;
-			cop /= 10;
-		}
-		if (rev == abs(n))
-return true;
-return false;
+	rev = 0;
+	while (cop) {
+		short a = cop % 10;
+		rev = rev * 10 + a;
+		cop /= 10;
+	}
+	if (rev == abs(n))
+		return true;
+	return false;
+}
+
+void PrintPalindrome(long long a, long long n) {
+	while (n <= a) {
+		if (isPalindrome(n))
+			cout << n << "  ";
+		n++;
+	}
+	cout << endl;
 }
