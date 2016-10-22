3a2e19f92e6f5c84383aae34d474da5c94a0dad0
antonandronchik <antonandron46@gmail.com>
2016-10-22 17:39:07 +0300

    Update Вывод трёхзначных чисел 1.cpp

--- Вывод трёхзначных чисел 1.cpp
+++ Вывод трёхзначных чисел 1.cpp
@@ -2,34 +2,40 @@
 
 using namespace std;
 
-bool isThreedigit (int);
+bool isThreedigit(int);
+
+void PrintNumbers(int);
 
 int main() {
-int i = 10;
-	while(i < 1000){
-		if (isThreedigit (i))
-			cout<< i << "  ";
-			i++;
-	}
-	cout<<endl;
+	int num = 10;
+	PrintNumbers(num);
 	return 0;
 }
 
-bool isThreedigit (int i) {
+bool isThreedigit(int i) {
 	int j = 0;
 	int a, cop;
-		cop = i;
-		while(j < 3) 
-		{
-			a = cop % 10;
-			if (a == 0)
-				break;
-			if (i % a == 0) 
-				cop /= 10;
-			else break;
-			j++;
-		}
+	cop = i;
+	while (j < 3)
+	{
+		a = cop % 10;
+		if (a == 0)
+			break;
+		if (i % a == 0)
+			cop /= 10;
+		else break;
+		j++;
+	}
 	if (j == 3)
 		return true;
 	return false;
+}
+
+void PrintNumbers(int num) {
+	while (num < 1000) {
+		if (isThreedigit(num))
+			cout << num << "  ";
+		num++;
+	}
+	cout << endl;
 }
