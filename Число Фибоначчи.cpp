861bffbe1a14ef6726f1ae58accb4628cf7e1676
antonandronchik <antonandron46@gmail.com>
2016-10-22 17:07:26 +0300

    Update Число Фибоначчи.cpp

--- Число Фибоначчи.cpp
+++ Число Фибоначчи.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
+
 using namespace std;
-long fibonachy(int);
+
+int fibonachy(int);
+
 int main() {
-	int m, i = 1;
+	int num, i = 0;
 	do {
-		m = fibonachy(i); i++;
-	} while (m < 99);
-	cout << "Min three-digit number of the Fibonachi sequence is " << m << endl;
+		num = fibonachy(i);
+		i++;
+	} while (num < 99);
+	cout << "Min three-digit number of the Fibonachi sequence is " << num << endl;
 	return 0;
 }
 
-long fibonachy(int a = 20)
-{
+int fibonachy(int a = 20) {
 	int i;
 	long s = 0, b = 1, c = 1;
 	for (i = 1; i <= (a - 2); i++) {
