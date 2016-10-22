3cb50cc8601afe82005e2e1c61bd495177c76c01
antonandronchik <antonandron46@gmail.com>
2016-10-22 19:26:43 +0300

    Update Последовательность цифр.cpp

--- Последовательность цифр.cpp
+++ Последовательность цифр.cpp
@@ -6,13 +6,14 @@ int main() {
 	short ok = true;
 	while (ok){
 	cout << "Please, Enter number" << endl;;
-	long long n;
+	long long n, num;
 	cin >> n;
-	int next, pref = n % 10;
-		n = n / 10;
+	num = fabs(n);
+	int next, pref = num % 10;
+		num = num / 10;
 		bool eq = false, less = false, grt = false;
-	while (n) {
-		next = n % 10;
+	while (num) {
+		next = num % 10;
 		if (next < pref)
 			grt = true;
 		else if (next > pref)
@@ -20,11 +21,11 @@ int main() {
 		else eq = true;
 
 		pref = next;
-		n /= 10;
+		num /= 10;
 	}
 	if (eq && !less && !grt) 
 		cout << "Monotone" << endl;
-	else if (eq && less && grt)
+	else if ((eq && less && grt)|| (!eq && less && grt))
 		cout << "Neither" << endl;
 	else if (less && eq && !grt)
 		cout << "Not strictly decreasing" << endl;
