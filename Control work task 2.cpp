971efc1a6f518669e5ca79679b0905a77a170ee0
antonandronchik <antonandron46@gmail.com>
2016-12-29 12:07:26 +0300

    Update Control work task 2.cpp

--- Control work task 2.cpp
+++ Control work task 2.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 int SearchNumber(int);
+void digits(int& temp, int& copy);
+int NewNumber(int number, int copy);
 
 int main()
 {
@@ -27,21 +29,33 @@ int SearchNumber(int number)
 		return -1;
 	else 
 	{ 
-		copy = number; 
-		int len = 0; 
-		while (copy % 10){ 
-			len++; 
-			if (copy % 10 < (copy % 100) / 10){ 
-				copy = number; 
-				int temp = 1; 
-				for (int i = 0; i < len - 1; i++) 
-					temp *= 10; 
-				int a = copy / (temp * 100), b = copy % temp, c = (copy / (temp * 10)) % 10, d = (copy / temp) % 10; 
-				copy = a * temp * 100 + b + d * temp * 10 + c * temp;  
-				return copy;
-				break; 
-			} 
-			copy /= 10; 
-		} 
+		NewNumber(number, copy);
 	} 
+}
+
+int NewNumber(int number, int copy)
+{	
+	copy = number; 
+	int len = 0; 
+	while (copy % 10)
+	{ 
+		len++; 
+		if (copy % 10 < (copy % 100) / 10)
+		{ 
+			int temp = 1;
+			copy = number;
+			for (int i = 0; i < len - 1; i++) 
+				temp *= 10;
+			digits(temp, copy);
+			return copy;
+		} 
+		copy /= 10; 
+	} 
+}
+
+void digits(int& temp, int& copy)
+{ 
+	int a = copy / (temp * 100), b = copy % temp, c = (copy / (temp * 10)) % 10, d = (copy / temp) % 10; 
+	copy = a * temp * 100 + b + d * temp * 10 + c * temp; 
+
 }
